933293f1358042213f33948bd1d1fbc437fd7eac
Bassel <bassela2@illinois.edu>
2020-04-27 18:56:24 -0500

    Added two activities, some code fro main activity

---
+++ app/src/main/java/com/example/imagetotext/Image_BoxEditor.java
@@ -0,0 +1,14 @@
+package com.example.imagetotext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Image_BoxEditor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image__box_editor);
+    }
+}
--- app/src/main/java/com/example/imagetotext/MainActivity.java
+++ app/src/main/java/com/example/imagetotext/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.imagetotext;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    public Button copyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        copyButton = (Button) findViewById(R.id.button3);
+
+        copyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, text_Copy.class);
+                startActivity(intent);
+            }
+        });
     }
 }
