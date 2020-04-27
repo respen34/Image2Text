dab6b4e360103d1ad2dc8a0757bec131640e8222
Bassel <bassela2@illinois.edu>
2020-04-27 18:56:24 -0500

    Added two activities, some code fro main activity
    (cherry picked from commit 933293f1358042213f33948bd1d1fbc437fd7eac)

---
+++ app/src/main/java/com/example/imagetotext/text_Copy.java
@@ -0,0 +1,14 @@
+package com.example.imagetotext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class text_Copy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text__copy);
+    }
+}
