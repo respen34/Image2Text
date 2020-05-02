f2d0390c4f91904ae986fbdb6d8330512ed90484
respen34 <sps7@illinois.edu>
2020-05-01 20:07:13 -0500

    Changed from tess4j to tess-two

--- app/src/main/java/com/example/imagetotext/Tesseract.java
+++ app/src/main/java/com/example/imagetotext/Tesseract.java
@@ -1,10 +1,15 @@
 package com.example.imagetotext;
 
 import java.io.File;
-import net.sourceforge.tess4j.*;
+import com.googlecode.tesseract.android.*;
 
 public class Tesseract {
-    public static Tesseract getTesseract() {
+    public static void getTesseract() {
+        return;
+    }
+
+    public static String convertImage(File image, int topLeftX, int topLeftY,
+                                      int bottomRightX, int bottomRightY) {
         return null;
     }
 }
